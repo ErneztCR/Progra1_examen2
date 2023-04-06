0e378ff89fa9f8fbbcd1e73339d97e6454b1f413
ErneztCR <49740650+ErneztCR@users.noreply.github.com>
2023-04-05 18:19:21 -0600

    se crea la clase Encuesta

---
+++ src/ernestovargasexamen2/Encuesta.java
@@ -0,0 +1,10 @@
+
+package ernestovargasexamen2;
+
+/**
+ *
+ * @author ervargas
+ */
+public class Encuesta {
+    
+}
--- src/ernestovargasexamen2/Formulario.java
+++ src/ernestovargasexamen2/Formulario.java
@@ -32,11 +32,11 @@ public class Formulario extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 599, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 613, Short.MAX_VALUE)
         );
 
         pack();

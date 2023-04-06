0d8b45baf88565a5fb93a588252fa31fbad6335d
ErneztCR <49740650+ErneztCR@users.noreply.github.com>
2023-04-05 19:24:05 -0600

    Se crea primera logica de encuesta.java

--- src/ernestovargasexamen2/Encuesta.java
+++ src/ernestovargasexamen2/Encuesta.java
@@ -6,5 +6,69 @@ package ernestovargasexamen2;
  * @author ervargas
  */
 public class Encuesta {
-    
+
+    //atributos
+    private int numEncuesta;
+    private String nombre;
+    private int edad;
+    private String email;
+    private boolean vehiculoPropio;
+
+    //constructor
+    public Encuesta(int numEncuesta, String nombre, int edad, String email, boolean vehiculoPropio) {
+        this.numEncuesta = numEncuesta;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+        this.vehiculoPropio = vehiculoPropio;
+    }
+
+    //getters y setters
+    public int getNumEncuesta() {
+        return numEncuesta;
+    }
+
+    public void setNumEncuesta(int numEncuesta) {
+        this.numEncuesta = numEncuesta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isVehiculoPropio() {
+        return vehiculoPropio;
+    }
+
+    public void setVehiculoPropio(boolean vehiculoPropio) {
+        this.vehiculoPropio = vehiculoPropio;
+    }
+
+    //toString
+    @Override
+
+    public String toString() {
+        return "Encuesta{" + "numEncuesta=" + numEncuesta + ", nombre=" + nombre + ", edad=" + edad + ", email=" + email + ", vehiculoPropio=" + vehiculoPropio + '}';
+    }
+        
 }
